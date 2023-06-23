769c0896149b5b9694a7cb766189091154f7c507
Adrien <adrien.troise@epitech.eu>
2023-06-23 15:34:09 +0200

    finish

--- tictactoe.py
+++ tictactoe.py
@@ -13,6 +13,10 @@ plateau = [" "]*9
 tour_de_X = True
 fin_du_jeu = False
 
+# Variables pour stocker les noms des joueurs
+joueur_X = ""
+joueur_O = ""
+
 # Fonction pour vérifier si un joueur a gagné ou si la partie est terminée
 def verifier_victoire():
     combinaisons_gagnantes = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
@@ -20,7 +24,11 @@ def verifier_victoire():
         if plateau[combinaison[0]] == plateau[combinaison[1]] == plateau[combinaison[2]] != " ":
             gagnant = joueur_X if plateau[combinaison[0]] == "X" else joueur_O
             messagebox.showinfo("Fin du jeu", "Le joueur " + gagnant + " a gagné !")
-            fenetre.quit()
+            if messagebox.askyesno("Nouvelle partie", "Voulez-vous jouer une nouvelle partie ?"):
+                fenetre.quit()
+                main()
+            else:
+                fenetre.quit()
     # Vérifier si toutes les positions du plateau sont remplies
     if " " not in plateau:
         messagebox.showinfo("Fin du jeu", "Fin de la partie, ex-aequo.")
@@ -36,7 +44,7 @@ def clic_bouton(index):
     if plateau[index] == " " and not fin_du_jeu:
         plateau[index] = "X" if tour_de_X else "O"
         labels[index].config(text=plateau[index], fg="red", font=("Helvetica", 24))
-        boutons[index].config(state="disabled")  # Disable the button after it's clicked
+        boutons[index].config(state="disabled")  
         verifier_victoire()
         tour_de_X = not tour_de_X
 
@@ -48,14 +56,18 @@ def main():
     plateau = [" "]*9
     tour_de_X = True
 
-    # Demande des noms des joueurs
-    joueur_X = ""
-    while not joueur_X:
+    if joueur_X and joueur_O and messagebox.askyesno("Nouvelle partie", "Même joueurs ?"):
+        pass
+    else:
         joueur_X = simpledialog.askstring("Input", "Quel est le nom du premier joueur (X) ?", parent=fenetre)
+        if joueur_X is None:  
+            fenetre.quit()
+            return
 
-    joueur_O = ""
-    while not joueur_O:
         joueur_O = simpledialog.askstring("Input", "Quel est le nom du deuxième joueur (O) ?", parent=fenetre)
+        if joueur_O is None:  
+            fenetre.quit()
+            return
 
     for i in range(9):
         fenetre.grid_rowconfigure(i//3, weight=1)
